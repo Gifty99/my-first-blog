dfd873388e5a86c8374c00ddf92e47841cd35add
Your Name <you@example.com>
2019-08-07 10:51:37 +0100

    My Django Girls app, first commit

---
+++ example1.py
@@ -0,0 +1,20 @@
+MyName="Gifty Takyiwaa"
+World='Hello World'
+print(World)
+def hi(okay):
+	print('My name is ' + MyName)
+	if okay==8:
+		print('It works!')
+	elif okay==(45<8):
+		print('It does\'nt work')
+	else:
+		print("Hey! Follow instructions! :(")	
+hi(8)
+def wossop(prim):
+	print('Hi ' + prim + '!')	
+Boys = ['Clinton', 'Amos', 'Manaf', 'Ivan', 'Ken', 'Fen']
+for prim in Boys:
+	wossop(prim)
+	print('Next please')	
+for m in range(0,100):
+	print(m)
